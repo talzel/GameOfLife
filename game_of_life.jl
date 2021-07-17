46c49b537c6182e615c3a041d784e38bef0275a1
talz <tal.zelig@planckresolution.com>
2021-07-17 12:15:19 +0300

    fixed ifs

--- game_of_life.jl
+++ game_of_life.jl
@@ -3,11 +3,10 @@
 
 
 function runGol(init_gen,generations::Int64)
-    # display(init_gen)
-    # println(generations)
     println("Generation 0:")
     display(init_gen)
     println()
+    
     prev = init_gen
     for x in [1:1:generations;]
         println("Generation $(x):")
@@ -16,8 +15,6 @@ function runGol(init_gen,generations::Int64)
         println()
         prev = new_generation
     end
-    # sleep(5)
-    # display(.!initial_configuration)
 end
 
 
@@ -25,21 +22,15 @@ function create_new_generation(current_generation::Matrix{Bool})::Matrix{Bool}
     next_generation = zeros(Bool,axes(current_generation))
     for i in CartesianIndices(current_generation)
         neighbours = Bool[]
-        if i[2] == axes(current_generation,2).stop
-            # no east update:
-        else
+        if !(i[2] == axes(current_generation,2).stop) #east update
             neighbours = [neighbours; current_generation[i + CartesianIndex(0,1)]] # east update
         end
 
-        if i[2] == 1
-            #no west update
-        else
+        if !(i[2] == 1) #no west update
             neighbours = [neighbours; current_generation[i + CartesianIndex(0,-1)]] # west update
         end
 
-        if i[1] == 1
-            # no north update
-        else 
+        if !(i[1] == 1) # no north update
             neighbours = [neighbours; current_generation[i + CartesianIndex(-1,0)]] # north update
             if !(i[2] == axes(current_generation,2).stop) # northeast update
                 neighbours = [neighbours; current_generation[i + CartesianIndex(-1,1)]]
@@ -62,7 +53,6 @@ function create_new_generation(current_generation::Matrix{Bool})::Matrix{Bool}
         end
 
         neighbours_count = length(neighbours[neighbours])
-        # println("neighbours $(neighbours)")
         
         # decide if index lives or dies next gen
         next_generation[i] = current_generation[i] ? neighbours_count in [2,3] : neighbours_count == 3
