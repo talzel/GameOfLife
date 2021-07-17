ade966f6dc6a35ae94479f804071936c9c2fccfa
talz <tal.zelig@planckresolution.com>
2021-07-17 12:12:28 +0300

    delete comments

--- game_of_life.jl
+++ game_of_life.jl
@@ -24,57 +24,39 @@ end
 function create_new_generation(current_generation::Matrix{Bool})::Matrix{Bool}
     next_generation = zeros(Bool,axes(current_generation))
     for i in CartesianIndices(current_generation)
-        # println("current index")
-        # println(i)
         neighbours = Bool[]
         if i[2] == axes(current_generation,2).stop
             # no east update:
         else
-            # println("check east")
-            # println(i)
             neighbours = [neighbours; current_generation[i + CartesianIndex(0,1)]] # east update
         end
 
         if i[2] == 1
             #no west update
         else
-            # println("check west")
-            # println(i)
             neighbours = [neighbours; current_generation[i + CartesianIndex(0,-1)]] # west update
         end
 
         if i[1] == 1
             # no north update
         else 
-            # println("check north")
-            # println(i)
             neighbours = [neighbours; current_generation[i + CartesianIndex(-1,0)]] # north update
             if !(i[2] == axes(current_generation,2).stop) # northeast update
-                # println("check north east")
-                # println(i)
                 neighbours = [neighbours; current_generation[i + CartesianIndex(-1,1)]]
             end
 
             if !(i[2] == 1) # northwest update
-                # println("check north west")
-                # println(i)
                 neighbours = [neighbours; current_generation[i + CartesianIndex(-1,-1)]]
             end
         end
 
         if !(i[1] == axes(current_generation,1).stop) # south check
-            # println("check south")
-            # println(i)
             neighbours = [neighbours; current_generation[i + CartesianIndex(1,0)]] # south update
             if !(i[2] == axes(current_generation,2).stop) # south east update
-                # println("check south east")
-                # println("$(i) -> $(i + CartesianIndex(1,1))")
                 neighbours = [neighbours; current_generation[i + CartesianIndex(1,1)]]
             end
 
             if !(i[2] ==  1) # south west update
-                # println("check south west")
-                # println("$(i) -> $(i + CartesianIndex(1,-1))")
                 neighbours = [neighbours; current_generation[i + CartesianIndex(1,-1)]]
             end
         end
