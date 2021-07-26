9ae6dee2257546b970022ec2c79f4c0cf78a8c6c
talz <tal.zelig@planckresolution.com>
2021-07-27 00:24:17 +0300

    readded missing files

---
+++ src/GameOfLife.jl
@@ -0,0 +1,8 @@
+module GameOfLife
+include("RunGol.jl")
+
+
+greet() = print("Hello World!")
+run(generation_0, generations) = RunGol.runGol(generation_0,generations)
+# runFromFile(fp) = x = readdlm(fp)
+# end # module
